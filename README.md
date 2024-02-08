ad7d89d25bcce34acc3f8c5eaec69933bf36b2d1
SytHamMer <mathys.lebon1@gmail.com>
2024-02-08 14:36:22 +0100

    [ADD]: README + start of the structure

---
+++ README.md
@@ -0,0 +1,12 @@
+# INFO834 – TP1 – Un cache Redis pour EtuServices
+## Github link:
+https://github.com/SytHamMer/INFO834-Cache-Redis
+
+## How to run it:
+
+## Technologies:
+- Redis
+- Python
+- html
+- php
+
---
+++ python/utilisateur.sql
@@ -0,0 +1,7 @@
+CREATE TABLE utilisateurs (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    last_name VARCHAR(255) NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
